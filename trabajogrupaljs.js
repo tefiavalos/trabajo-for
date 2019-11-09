762fa25a34a6f116c88d53e84e1ccfdcb30f0369
tefiavalos <emlavalos92@gmail.com>
2019-11-08 22:22:10 -0300

    trabajo

---
+++ trabajogrupaljs.js
@@ -0,0 +1,40 @@
+//_______ejercicio5________
+
+/*let suma = 0
+
+for(i=0; i<=5; i++){
+    suma=suma+i
+    console.log(suma)
+};*/
+
+//_______ejercicio6________
+/*let sumaDos = 0
+
+for(i=1; i<=1000; i+=2){
+    sumaDos=sumaDos+i
+    console.log(sumaDos)
+}*/
+
+//_______ejercicio7________
+/*let acumuladora = "*"
+
+for(i=1; i<=13; i++){
+    console.log(acumuladora)
+    acumuladora = acumuladora + "*"
+  
+}*/
+
+//______Ejercicio8_________
+
+let acumuladora = "*"
+
+for(i=1; i<=13; i+=2){
+
+    console.log(acumuladora)
+    acumuladora = acumuladora + "**"
+  
+}
+
+
+
+
